0cc369d4e4cfcf4aa12e3720b68ecbc918ddf5d6
Alex Morega <alex@grep.ro>
2024-07-21 02:00:47 +0300

    Install django_htmx

--- xtalk/settings.py
+++ xtalk/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -28,6 +29,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = "xtalk.urls"
