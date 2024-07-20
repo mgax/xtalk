27cd32ecbc69c669b7034308fe9ccce9fff6cf9c
Alex Morega <alex@grep.ro>
2024-07-21 01:12:29 +0300

    Install django-browser-reload

--- xtalk/urls.py
+++ xtalk/urls.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if settings.BROWSER_RELOAD:
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
